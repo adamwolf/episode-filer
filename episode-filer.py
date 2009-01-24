f8343a6a95eb60f71199e427aa21409d9a98cb14
Administrator <adamwolf@feelslikeburning.com>
2009-01-24 16:52:27 -0600

    Added GPL information.  Added todos and a rudimentary changelog to the README.

--- episode-filer.py
+++ episode-filer.py
@@ -4,9 +4,24 @@ episode-filer.py
 
 episode-filer organizes your tv shows!  
 
-Adam Wolf
-http://feelslikeburning.com
+http://feelslikeburning.com/projects/episode-filer/
 """
+#    Copyright 2009 Adam Wolf 
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 
 import tvorg, logging, os, sys, shutil
 from optparse import OptionParser
@@ -167,7 +182,7 @@ def move_file(episode, show, options):
             return
 
     if options.pretend:  
-        logging.info("Not moving because --pretend set.")
+        #logging.info("Not moving because --pretend set.")
         return
 
     logging.debug("Really moving %s to %s" % (episode.path, destination_path))
--- names-detector.py
+++ names-detector.py
@@ -4,9 +4,24 @@ names-detector.py
 Detect "clean tv show names" of any tv show files it can parse from the
 specified directory.
 
-Adam Wolf
-http://feelslikeburning.com
+http://feelslikeburning.com/projects/episode-filer
 """
+#    Copyright 2009 Adam Wolf
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 
 import tvorg, logging
 import os
